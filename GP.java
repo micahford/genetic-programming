0df5782c9a4c2550f01033dbd4ee00788f6db945
Micah Brown <micahbrown93@gmail.com>
2015-02-09 22:21:12 -0500

    Update GP.java
    now you can run different parameters on the same tree and see the different values.
    -added a method to replace value "x" in tree with value "y".

--- GP.java
+++ GP.java
@@ -5,15 +5,12 @@ import java.util.Stack;
 
 //represents a tree.
 public class GP {
-	private Node<String> root;
 	private static int maxdepth = 2;
 	private static Stack<String> valueStack;
-	private static int x = 2;
+	private static Node<String> rootNode;
 
 	public GP(String rootName) {
-		root = new Node<String>();
-		root.value = rootName;
-		root.children = new Node[2];
+
 	}
 
 	public static class Node<T> {
@@ -30,10 +27,10 @@ public class GP {
 	}
 	
 	//returns a string representation of a random double between -5 and +5, or x.
-	private static String randNum() {
+	private static String randNum(int x) {
 		Random r = new Random();
 		if(r.nextDouble()<0.5){
-			return Integer.toString(x);
+			return "x";
 		}
 		else {
 			return String.format("%.2f",r.nextInt(10)-5 + r.nextDouble());
@@ -41,36 +38,36 @@ public class GP {
 		
 	}
 
-	public Node<String> createBinaryTree(){
-		Node<String> rootNode = new Node<String>();
+	public Node<String> createBinaryTree(int param){
+		rootNode = new Node<String>();
 		rootNode.depthFromRoot = 0;
 		rootNode.value =randOperator();
 		
 		Node<String> curr = rootNode;
-		treeHelper(curr, curr.depthFromRoot);
+		treeHelper(curr, curr.depthFromRoot, param);
 		return rootNode;
 		
 	}
 	
-	public static void treeHelper(Node<String> currentNode, int depth){
+	public static void treeHelper(Node<String> currentNode, int depth, int x){
 		if(depth == maxdepth){
 			return;
 		}else{
 			Node<String> n1 = new Node<String>();  
-			if(depth == maxdepth-1){n1.value = randNum();}
+			if(depth == maxdepth-1){n1.value = randNum(x);}
 			else {n1.value = randOperator();}	
 			n1.parent = currentNode;
 			n1.depthFromRoot = currentNode.depthFromRoot+1;
 			currentNode.children[0] = n1;
-			treeHelper(n1, n1.depthFromRoot);
+			treeHelper(n1, n1.depthFromRoot, x);
 			
 			Node<String> n2 = new Node<String>();
-			if(depth == maxdepth-1){n2.value = randNum();}
+			if(depth == maxdepth-1){n2.value = randNum(x);}
 			else {n2.value = randOperator();}			
 			n2.parent = currentNode;
 			n2.depthFromRoot = currentNode.depthFromRoot+1;
 			currentNode.children[1] = n2;
-			treeHelper(n2, n2.depthFromRoot);
+			treeHelper(n2, n2.depthFromRoot, x);
 		
 		}
 	}
@@ -82,7 +79,7 @@ public class GP {
 	}
 	
 	//performs inorder traversal, prints off (sideways).
-	public static void printHelper(Node<String> currentNode, int depth){
+	private static void printHelper(Node<String> currentNode, int depth){
 		if(currentNode == null){
 			return;
 		}else{
@@ -99,15 +96,39 @@ public class GP {
 	}
 
 	//This method actually calculates the value of a tree. It takes the head node of the tree as input.
-	private static double valueOfTree(Node<String> head){
+	private static double valueOfTree(Node<String> head, int x){
 		valueStack = new Stack<String>(); //resets the "valueStack"
+		change_node(head, "x", Integer.toString(x)); //change all the blank x's to the string representation of the parameter for the function.
+		
 		valueHelper(head, 0); //Now, "loads" the value stack.
 		reverseStack(); //Now, reverses the value stack.
 
-		double total = calculateStack(); //now, convert from postfix (valueStack) to an infix and calculate.
+		double total = calculateStack(); //now, convert from postfix (valueStack) to an infix and calculate.\
+		printTree(head);
+		change_node(head, Integer.toString(x), "x"); //now, change all the parameter values back to x's (so that the same tree can be called for different values of the paremeter).
 		return total;
 	}
 	
+	//let's go through tree and change all the "old values" to the "new values".
+	private static void change_node(Node<String> head, String oldValue, String newValue) {
+		change_node_helper(rootNode, oldValue, newValue);
+	}
+	private static void change_node_helper(Node<String> currentNode, String oldValue, String newValue){
+		if (currentNode == null) {
+			return;
+		} else {
+
+			//call method on right child.
+			change_node_helper(currentNode.children[1], oldValue, newValue);
+			//**make actual replacement** from old value to new value.
+			if(currentNode.value.equals(oldValue)) currentNode.value = newValue;
+			
+			// now call method on left child
+			change_node_helper(currentNode.children[0], oldValue, newValue);
+		}
+
+	}
+
 	//reverses valueStack. very simple.
 	private static void reverseStack(){
 		Queue<String> tempQueue = new LinkedList<String>();
@@ -184,9 +205,15 @@ public class GP {
 	public static void main(String args[]) {
 		GP gp = new GP("+");	
 		
-		Node<String> foo = gp.createBinaryTree();
-		printTree(foo);
-		System.out.println("value of tree: "+valueOfTree(foo));
+		int x = 2; // **the parameter to our tree**.
+		
+		Node<String> foo = gp.createBinaryTree(x);
+		printTree(foo); //prints tree with x's.
+		System.out.printf("value of tree for parameter %d : %f\n", 2, valueOfTree(foo, 2));
+		//printTree(foo);
+		System.out.printf("value of tree for parameter %d : %f\n", 3, valueOfTree(foo, 3));
+		//printTree(foo);
+
 
 		
 	}
